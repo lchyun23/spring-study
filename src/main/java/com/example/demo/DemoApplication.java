504c67611835f1cf865e2f770f6534cc75ff2aeb
Aaron Ryu <fbcndah@gmail.com>
2024-09-24 21:15:46 +0900

    실습 0-7: 예상하지 못한 예외 상황 발생을 대비하여 Exception Catch 필요

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -20,6 +20,9 @@ public class DemoApplication {
         if (!(username.equals("admin") && password.equals("1234"))) {
             throw new DatabaseException();
         }
+        if (true/* 강제로 예외를 발생시키기 위해 if (true) 추가 - 그렇지않으면 '아래부터 실행되지 않는데 코드가 있다'는 빌드 오류 */) {
+            throw new RuntimeException("Cannot connect to the Database Server");
+        }
         log.info("- Database is connected successfully.");
     }
 
@@ -35,6 +38,8 @@ public class DemoApplication {
             log.warn("유저가 잘못된 비밀번호를 입력 중", e);
         } catch (DatabaseException e) {
             log.warn("잘못된 유저 정보로인한 데이터베이스 접속 실패", e);
+        } catch (Exception e) {
+            log.error("데이터베이스 내 알 수 없는 오류 발생 - 원인 분석 및 대응 필요", e);
         }
     }
 }
