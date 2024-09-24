7468bf4c16c8d292e796a02712b0c9c3e8b716ff
Aaron Ryu <fbcndah@gmail.com>
2024-09-24 21:11:47 +0900

    실습 0-6: 예외 상황별 Exception 클래스 만들기 + 다중 Exception 클래스 Catch

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,27 +1,40 @@
 package com.example.demo;
 
+import com.example.demo.exception.DatabaseException;
+import com.example.demo.exception.PasswordException;
+import com.example.demo.exception.UsernameException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.sql.SQLException;
+import org.springframework.util.StringUtils;
 
 @Slf4j
 @SpringBootApplication
 public class DemoApplication {
     static void connect(String username, String password) {
-        try {
-            if (username.equals("admin") && password.equals("1234")) {
-                System.out.println("- Database is connected successfully.");
-            } else {
-                throw new SQLException("데이터베이스 접속 실패");
-            }
-        } catch (SQLException e) {
-            log.error(String.format("데이터베이스 계정 불일치 - username: %s, password: %s", username, password));
+        if (!StringUtils.hasLength(username)) {
+            throw new UsernameException();
         }
+        if (!StringUtils.hasLength(password)) {
+            throw new PasswordException();
+        }
+        if (!(username.equals("admin") && password.equals("1234"))) {
+            throw new DatabaseException();
+        }
+        log.info("- Database is connected successfully.");
     }
 
     public static void main(String[] args) {
-        connect("admin", "1234");
-        connect("user", "9876");
+        try {
+            connect(null, "9876");
+            connect("user", null);
+            connect("user", "9876");
+            connect("admin", "1234");
+        } catch (UsernameException e) {
+            log.warn("유저가 존재하지 않는 아이디를 입력 중", e);
+        } catch (PasswordException e) {
+            log.warn("유저가 잘못된 비밀번호를 입력 중", e);
+        } catch (DatabaseException e) {
+            log.warn("잘못된 유저 정보로인한 데이터베이스 접속 실패", e);
+        }
     }
 }
