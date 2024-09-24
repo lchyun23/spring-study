0e7500c2f304ebcd306d7da136639de012ae5e4f
Aaron Ryu <fbcndah@gmail.com>
2024-09-25 00:36:45 +0900

    실습 1-11: DTO(Data Transfer Object)를 생성자로 받아 필요한 필드만 사용

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,26 +2,19 @@ package com.example.demo;
 
 import com.example.demo.member.Administrator;
 import com.example.demo.member.Member;
+import com.example.demo.member.MemberRequestDto;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
-        Member aaron = new Member("Aaron");
-        aaron.setEmail("aaron@example.com");
-        Member baron = new Member("Baron");
-        aaron.setEmail("baron@example.com");
+        MemberRequestDto dto = new MemberRequestDto("Aaron", "aaron@example.com");
+        Member aaron = new Member(dto);
 
         System.out.println("---");
         System.out.println(aaron);              // 객체
         System.out.println(aaron.toString());   // 객체
         System.out.println(aaron.getName());    // 필드 중 name
 //      System.out.println(aaron.name);         // 필드 중 name (접근제어자 private)
-
-        System.out.println("---");
-        System.out.println(baron);              // 객체
-        System.out.println(baron.toString());   // 객체
-        System.out.println(baron.getName());    // 필드 중 name
-//      System.out.println(baron.name);         // 필드 중 name (접근제어자 private)
     }
 }
