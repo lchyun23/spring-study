78538f9cbd4e6ac9588b6e85453503d912b2fac6
Aaron Ryu <fbcndah@gmail.com>
2024-09-24 20:44:24 +0900

    실습 0-4: Checked Exception 처리방법 2.2) 받기 (서버 오류 미발생)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,15 +6,19 @@ import java.sql.SQLException;
 
 @SpringBootApplication
 public class DemoApplication {
-    static void connect(String username, String password) throws SQLException {
-        if (username.equals("admin") && password.equals("1234")) {
-            System.out.println("- Database is connected successfully.");
-        } else {
-            throw new SQLException("데이터베이스 접속 실패");
+    static void connect(String username, String password) {
+        try {
+            if (username.equals("admin") && password.equals("1234")) {
+                System.out.println("- Database is connected successfully.");
+            } else {
+                throw new SQLException("데이터베이스 접속 실패");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         connect("admin", "1234");
         connect("user", "9876");
     }
