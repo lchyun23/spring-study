afc0f67d033c76455fa774ecbcd3e500f190202e
Aaron Ryu <fbcndah@gmail.com>
2024-09-24 22:53:46 +0900

    실습 0-11: 여러 계층서 Exception 발생 시 - (3) 중간에 받아서 처리하기

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.exception.CustomException;
 import com.example.demo.travel.TravelService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,10 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
     public static void main(String[] args) {
         TravelService service = new TravelService();
-        try {
-            service.reservation();
-        } catch (Exception e) {
-            log.error("예약 시 에러 발생 - (1) 한곳에서 다 처리하기", e);
-        }
+        service.reservation();
     }
 }
--- src/main/java/com/example/demo/travel/TravelService.java
+++ src/main/java/com/example/demo/travel/TravelService.java
@@ -22,14 +22,10 @@ public class TravelService {
             log.info("여행 패키지 예약 완료 (항공권 + 리조트 + 수영장)");
         } catch (UsernameException | PasswordException | DatabaseException e) {
             /* 1) 내부 고객(개발자)에게 표기할 에러메세지 */
-            log.error("여행 패키지 예약 실패 - (2) 중간에 받아서 다시 넘기기", e);
-            /* 2) 외부 고객(사용자)에게 표기할 에러메세지 */
-            throw new CustomException("RESERVATION_FAILED", 2, "여행 패키지 예약 실패");
+            log.error("여행 패키지 예약 실패 - (3) 중간에 받아서 처리하기", e);
         } catch (RuntimeException e) {
             /* 1) 내부 고객(개발자)에게 표기할 에러메세지 */
-            log.error("여행 패키지 예약 시 알 수 없는 에러 발생 - (2) 중간에 받아서 다시 넘기기", e);
-            /* 2) 외부 고객(사용자)에게 표기할 에러메세지 */
-            throw new CustomException("UNKNOWN_ERROR", 0, "여행 패키지 예약 시 알 수 없는 에러 발생");
+            log.error("여행 패키지 예약 시 알 수 없는 에러 발생 - (3) 중간에 받아서 처리하기", e);
         }
     }
 }
