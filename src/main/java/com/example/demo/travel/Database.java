8736ff7a1df82c12665ce4a65444f7204a39e3ca
Aaron Ryu <fbcndah@gmail.com>
2024-09-24 22:51:38 +0900

    실습 0-10: 여러 계층서 Exception 발생 시 - (2) 중간에 받아서 다시 넘기기

--- src/main/java/com/example/demo/travel/Database.java
+++ src/main/java/com/example/demo/travel/Database.java
@@ -1,9 +1,6 @@
 package com.example.demo.travel;
 
 import com.example.demo.exception.CustomException;
-import com.example.demo.exception.DatabaseException;
-import com.example.demo.exception.PasswordException;
-import com.example.demo.exception.UsernameException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
 
@@ -11,16 +8,16 @@ import org.springframework.util.StringUtils;
 public class Database {
     public Database(String username, String password) {
         if (!StringUtils.hasLength(username)) {
-            throw new UsernameException();
+            throw new CustomException("EMPTY_USERNAME", 2, "데이터베이스 접속에 필요한 Username 입력은 필수입니다.");
         }
         if (!StringUtils.hasLength(password)) {
-            throw new PasswordException();
+            throw new CustomException("EMPTY_PASSWORD", 2, "데이터베이스 접속에 필요한 Password 입력은 필수입니다.");
         }
         if (!(username.equals("admin") && password.equals("1234"))) {
-            throw new DatabaseException();
+            throw new CustomException("LOGIN_FAILED", 1, "데이터베이스 접속 실패");
         }
         if (true/* 강제로 예외를 발생시키기 위해 if (true) 추가 - 그렇지않으면 '아래부터 실행되지 않는데 코드가 있다'는 빌드 오류 */) {
-            throw new RuntimeException("Cannot connect to the Database Server");
+            throw new CustomException("CONNECTION_ERROR", 0, "Cannot connect to the Database Server");
         }
         log.info("- Database is connected successfully.");
     }
--- src/main/java/com/example/demo/travel/TravelService.java
+++ src/main/java/com/example/demo/travel/TravelService.java
@@ -1,19 +1,35 @@
 package com.example.demo.travel;
 
+import com.example.demo.exception.CustomException;
+import com.example.demo.exception.DatabaseException;
+import com.example.demo.exception.PasswordException;
+import com.example.demo.exception.UsernameException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class TravelService {
     public void reservation() {
-        Database connection = new Database(null, "9876");
-//      Database connection = new Database("user", null);
-//      Database connection = new Database("user", "9876");
-//      Database connection = new Database("admin", "1234");
+        try {
+            Database connection = new Database(null, "9876");
+//          Database connection = new Database("user", null);
+//          Database connection = new Database("user", "9876");
+//          Database connection = new Database("admin", "1234");
 
-        connection.save("항공권");
-        connection.save("리조트");
-        connection.save("수영장");
+            connection.save("항공권");
+            connection.save("리조트");
+            connection.save("수영장");
 
-        log.info("여행 패키지 예약 완료 (항공권 + 리조트 + 수영장)");
+            log.info("여행 패키지 예약 완료 (항공권 + 리조트 + 수영장)");
+        } catch (UsernameException | PasswordException | DatabaseException e) {
+            /* 1) 내부 고객(개발자)에게 표기할 에러메세지 */
+            log.error("여행 패키지 예약 실패 - (2) 중간에 받아서 다시 넘기기", e);
+            /* 2) 외부 고객(사용자)에게 표기할 에러메세지 */
+            throw new CustomException("RESERVATION_FAILED", 2, "여행 패키지 예약 실패");
+        } catch (RuntimeException e) {
+            /* 1) 내부 고객(개발자)에게 표기할 에러메세지 */
+            log.error("여행 패키지 예약 시 알 수 없는 에러 발생 - (2) 중간에 받아서 다시 넘기기", e);
+            /* 2) 외부 고객(사용자)에게 표기할 에러메세지 */
+            throw new CustomException("UNKNOWN_ERROR", 0, "여행 패키지 예약 시 알 수 없는 에러 발생");
+        }
     }
 }
