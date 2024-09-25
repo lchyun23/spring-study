54e836b343e9ab03163a9f1dbfe84b3012acf2f7
Aaron Ryu <fbcndah@gmail.com>
2024-09-25 19:47:39 +0900

    실습 1-17: (심화) 빌더(@Builder) 사용 시 - (1) 클래스 단위가 아닌 생성자 단위 사용

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,30 +6,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
-        String request = "AARON";
-
-        // (1) 빌더 정의
-        // (2) 필드 설정 : (2.1) 공통 부분을 먼저 설정하고
-        Member.MemberBuilder builder = Member.builder()
-                .age(10)
-                .email("common@example.com");
-        // (2) 필드 설정 : (2.2) 다른 부분은 따로 설정한다 (조건부)
-        if (request.equals("AARON")) {
-            builder
-                    .id(1)
-                    .name("Aaron");
-        } else if (request.equals("BARON")) {
-            builder
-                    .id(2)
-                    .name("Baron");
-        }
-        // (3) 객체 생성
-        Member aaron = builder.build();
+        Member aaron = Member.builder()
+                .name("Aaron")
+                .email("aaron@example.com")
+                .build();
+        Member baron = new Member("Baron", "baron@example.com");
 
         System.out.println("---");
         System.out.println(aaron);              // 객체
         System.out.println(aaron.toString());   // 객체
         System.out.println(aaron.getName());    // 필드 중 name
 //      System.out.println(aaron.name);         // 필드 중 name (접근제어자 private)
+
+        System.out.println("---");
+        System.out.println(baron);              // 객체
+        System.out.println(baron.toString());   // 객체
+        System.out.println(baron.getName());    // 필드 중 name
+//      System.out.println(baron.name);         // 필드 중 name (접근제어자 private)
     }
 }
--- src/main/java/com/example/demo/member/Member.java
+++ src/main/java/com/example/demo/member/Member.java
@@ -2,17 +2,15 @@ package com.example.demo.member;
 
 import lombok.Builder;
 
-@Builder
 public class Member {
     protected Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
     protected String name;
     protected int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
     protected String email;
 
-    public Member(Integer id, String name, int age, String email) {
-        this.id = id;
+    @Builder
+    public Member(String name, String email) {
         this.name = name;
-        this.age = age;
         this.email = email;
     }
 
