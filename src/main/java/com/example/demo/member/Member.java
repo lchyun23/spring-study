045ecdd71c8bd8daaac4dcc160bc8ff452ac79a4
Aaron Ryu <fbcndah@gmail.com>
2024-09-29 16:40:02 +0900

    실습 2-22: @EqualsAndHashCode - equals, hashCode 비교 메서드 생성

--- src/main/java/com/example/demo/member/Member.java
+++ src/main/java/com/example/demo/member/Member.java
@@ -5,6 +5,7 @@ import lombok.*;
 @Getter
 @Setter
 @ToString(onlyExplicitlyIncluded = true, includeFieldNames = false)
+@EqualsAndHashCode
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class Member {
     @ToString.Include(name = "memberId")
@@ -14,16 +15,16 @@ public class Member {
     private int age;
     private String email;
 
-    public boolean equals(Member member) {
-        return this.hashCode() == member.hashCode();
-    }
-
-    public int hashCode() {
-        int hash = 7;
-        hash = 31 * hash + (id == null ? 0 : id.hashCode());
-        hash = 31 * hash + (name == null ? 0 : name.hashCode());
-        hash = 31 * hash + (int) age;
-        hash = 31 * hash + (email == null ? 0 : email.hashCode());
-        return hash;
-    }
+//  public boolean equals(Member member) {
+//      return this.hashCode() == member.hashCode();
+//  }
+//
+//  public int hashCode() {
+//      int hash = 7;
+//      hash = 31 * hash + (id == null ? 0 : id.hashCode());
+//      hash = 31 * hash + (name == null ? 0 : name.hashCode());
+//      hash = 31 * hash + (int) age;
+//      hash = 31 * hash + (email == null ? 0 : email.hashCode());
+//      return hash;
+//  }
 }
