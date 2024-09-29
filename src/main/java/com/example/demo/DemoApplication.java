cafef21f5e57724e5ef238fd7c2ab3fc396c014f
Aaron Ryu <fbcndah@gmail.com>
2024-09-29 15:15:16 +0900

    실습 2-13: 클래스 상속 시 @AllArgsConstructor 사용 불가 + toString 재정의

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.member.Administrator;
 import com.example.demo.member.Member;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
-        Member aaron = new Member(1, "Aaron", 10, "aaron@example.com");
+        Administrator aaron = new Administrator(1, "Aaron", 10, "aaron@example.com", "DEVELOPER");
 
         System.out.println("---");
         System.out.println(aaron);              // 객체
