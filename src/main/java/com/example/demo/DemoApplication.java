5f49a1d663e081fd5f0ba283a17c0caa63f342cc
Aaron Ryu <fbcndah@gmail.com>
2024-09-29 13:20:26 +0900

    실습 1-24: Singleton 싱글턴 객체를 위한 정적 팩토리 메서드

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,21 +9,22 @@ import java.util.List;
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
-        MemberCreateRequestDto requestDto = new MemberCreateRequestDto("Baron", "baron@example.com");
+        Member aaron = Member.getInstance();
+        Member baron = Member.getInstance();
 
-//      Member aaron = new Member(1, "Aaron", 10, "aaron@example.com"); // 생성자라 Private 가 되었으므로 외부에서 호출이 불가하다.
-        Member baron = Member.from(requestDto);
-
-//      System.out.println("---");
-//      System.out.println(aaron);              // 객체
+        System.out.println("---");
+        System.out.println(aaron);              // 객체 주소 (aaron 주소값 == baron 주소값)
 //      System.out.println(aaron.toString());   // 객체
 //      System.out.println(aaron.getName());    // 필드 중 name
 //      System.out.println(aaron.name);         // 필드 중 name (접근제어자 private)
 
         System.out.println("---");
-        System.out.println(baron);              // 객체
-        System.out.println(baron.toString());   // 객체
-        System.out.println(baron.getName());    // 필드 중 name
+        System.out.println(baron);              // 객체 주소 (aaron 주소값 == baron 주소값)
+//      System.out.println(baron.toString());   // 객체
+//      System.out.println(baron.getName());    // 필드 중 name
 //      System.out.println(baron.name);         // 필드 중 name (접근제어자 private)
+
+        System.out.println("---");
+        System.out.println(aaron == baron);
     }
 }
--- src/main/java/com/example/demo/member/Member.java
+++ src/main/java/com/example/demo/member/Member.java
@@ -1,30 +1,23 @@
 package com.example.demo.member;
 
-import com.example.demo.dto.MemberCreateRequestDto;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+import java.util.Objects;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Member {
+    private static Member instance;
+
     protected Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
     protected String name;
     protected int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
     protected String email;
 
-    public static Member from(MemberCreateRequestDto requestDto) {
-        return new Member(
-                null,
-                requestDto.getName(),
-                0,
-                requestDto.getEmail()
-        );
-    }
-
-    public String toString() {
-        return String.format("Member(id=%s, name=%s, age=%s, email=%s)", id, name, age, email);
-    }
-
-    public String getName() {
-        return this.name;
+    public static Member getInstance() {
+        if (Objects.isNull(instance)) {
+            instance = new Member();
+        }
+        return instance;
     }
 }
