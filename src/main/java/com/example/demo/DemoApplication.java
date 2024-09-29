fdf2e6455324c4aa19f60a460b220d305f540ab2
Aaron Ryu <fbcndah@gmail.com>
2024-09-29 17:28:21 +0900

    실습 2-32: @Data - 주의 : Final/Non-Final 필드 구별하여 생성자/수정자 호출

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,15 +8,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
     public static void main(String[] args) {
         Administrator aaron = new Administrator(1, "Aaron", 12, "aaron@example.com", "DEVELOPER");
-        Administrator baron = new Administrator(2, "Baron", 36, "baron@example.com", "DEVELOPER");
+        Administrator baron = new Administrator(1, "Aaron", 12, "aaron@example.com", "DEVELOPER");
 
         System.out.println("---");
         System.out.println(aaron);              // 객체
         System.out.println(aaron.toString());   // 객체
+        System.out.println(aaron.getName());    // @Getter (@Data = 모든 "FINAL" 필드에 대해 Getter 생성)
+//      aaron.setName("Caron");                 // @Setter (@Data = 모든 "NON-FINAL" 필드에 대해 Setter 생성) - 어떤것도 호출 불가
 
         System.out.println("---");
         System.out.println(baron);              // 객체
         System.out.println(baron.toString());   // 객체
+        System.out.println(baron.getName());    // @Getter (@Data = 모든 "FINAL" 필드에 대해 Getter 생성)
+//      baron.setName("Daron");                 // @Setter (@Data = 모든 "NON-FINAL" 필드에 대해 Setter 생성) - 어떤것도 호출 불가
 
         System.out.println("---");
         System.out.println(aaron == baron);
--- src/main/java/com/example/demo/member/Member.java
+++ src/main/java/com/example/demo/member/Member.java
@@ -4,10 +4,10 @@ import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 @Data
-@FieldDefaults(level = AccessLevel.PRIVATE)
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class Member {
-    /* private */ Integer id;
-    /* private */ String name;
-    /* private */ int age;
-    /* private */ String email;
+    /* private final */ Integer id;
+    /* private final */ String name;
+    /* private final */ int age;
+    /* private final */ String email;
 }
