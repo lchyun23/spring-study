efc6050602e37a405ef89600e01caedea7408958
Aaron Ryu <fbcndah@gmail.com>
2024-09-29 17:24:03 +0900

    실습 2-31: @Data - 주의 : @All 이 아닌 @RequiredArgsConstructor 추가

--- src/main/java/com/example/demo/member/Member.java
+++ src/main/java/com/example/demo/member/Member.java
@@ -3,32 +3,11 @@ package com.example.demo.member;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
-@Getter
-@Setter
-@ToString(onlyExplicitlyIncluded = true, includeFieldNames = false)
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Member {
-    @ToString.Include(name = "memberId")
-    @EqualsAndHashCode.Include
     /* private */ Integer id;
-    @ToString.Include(rank = -1)
-    @EqualsAndHashCode.Include
     /* private */ String name;
     /* private */ int age;
     /* private */ String email;
-
-//  public boolean equals(Member member) {
-//      return this.hashCode() == member.hashCode();
-//  }
-//
-//  public int hashCode() {
-//      int hash = 7;
-//      hash = 31 * hash + (id == null ? 0 : id.hashCode());
-//      hash = 31 * hash + (name == null ? 0 : name.hashCode());
-//      hash = 31 * hash + (int) age;
-//      hash = 31 * hash + (email == null ? 0 : email.hashCode());
-//      return hash;
-//  }
 }
