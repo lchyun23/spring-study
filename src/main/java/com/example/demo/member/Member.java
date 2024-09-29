6357cde0226dc9c57fc96e69faa0f2edbdcba252
Aaron Ryu <fbcndah@gmail.com>
2024-09-29 13:24:28 +0900

    실습 1-26: Race Condition 해결 - 2) 변수 생성 단위 Blocking : volatile + synchronized

--- src/main/java/com/example/demo/member/Member.java
+++ src/main/java/com/example/demo/member/Member.java
@@ -7,16 +7,18 @@ import java.util.Objects;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Member {
-    private static Member instance;
+    private volatile static Member instance;
 
     protected Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
     protected String name;
     protected int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
     protected String email;
 
-    public synchronized static Member getInstance() {
-        if (Objects.isNull(instance)) {
-            instance = new Member();
+    public static Member getInstance() {
+        synchronized (Member.class) {
+            if (Objects.isNull(instance)) {
+                instance = new Member();
+            }
         }
         return instance;
     }
