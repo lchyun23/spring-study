1fbd6b48a926c7a4f4f47bb62280ff3e053c7932
Aaron Ryu <fbcndah@gmail.com>
2024-09-29 17:03:29 +0900

    실습 2-29: @EqualsAndHashCode - 상속 시 부모-자식 모두 비교 (해결)

--- src/main/java/com/example/demo/member/Administrator.java
+++ src/main/java/com/example/demo/member/Administrator.java
@@ -6,7 +6,7 @@ import lombok.ToString;
 import java.time.LocalDate;
 
 @ToString(callSuper = true)
-@EqualsAndHashCode
+@EqualsAndHashCode(callSuper = true)
 public class Administrator extends Member {
     private String role;
     private LocalDate allocatedAt;
