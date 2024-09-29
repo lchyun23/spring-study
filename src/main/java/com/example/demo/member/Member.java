fd61ba824707591460a78da2d262dbfe77a1e8d9
Aaron Ryu <fbcndah@gmail.com>
2024-09-29 14:57:47 +0900

    실습 2-7: @ToString - 객체 내 필드들을 한번에 출력

--- src/main/java/com/example/demo/member/Member.java
+++ src/main/java/com/example/demo/member/Member.java
@@ -3,6 +3,7 @@ package com.example.demo.member;
 import lombok.*;
 
 @Getter
+@ToString
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class Member {
     private Integer id;
@@ -11,10 +12,6 @@ public class Member {
     private int age;
     @Setter
     private String email;
-
-    public String toString() {
-        return String.format("Member(id=%s, name=%s, age=%s, email=%s)", id, name, age, email);
-    }
 
 //  public Member(Integer id, String name, int age, String email) {
 //      this.id = id;
