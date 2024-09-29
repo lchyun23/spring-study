6a9e8147be1c798ed0218c6d776f2e27e4bb2aef
Aaron Ryu <fbcndah@gmail.com>
2024-09-29 18:37:28 +0900

    실습 1-28: Final 필드 - 한번 생성자로 설정된 필드값은 Setter 호출 불가

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,30 +1,19 @@
 package com.example.demo;
 
-import com.example.demo.dto.MemberCreateRequestDto;
 import com.example.demo.member.Member;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
-        Member aaron = Member.getInstance();
-        Member baron = Member.getInstance();
+        Member aaron = new Member(1, "Aaron");
 
         System.out.println("---");
         System.out.println(aaron);              // 객체 주소 (aaron 주소값 == baron 주소값)
-//      System.out.println(aaron.toString());   // 객체
-//      System.out.println(aaron.getName());    // 필드 중 name
-//      System.out.println(aaron.name);         // 필드 중 name (접근제어자 private)
-
-        System.out.println("---");
-        System.out.println(baron);              // 객체 주소 (aaron 주소값 == baron 주소값)
-//      System.out.println(baron.toString());   // 객체
-//      System.out.println(baron.getName());    // 필드 중 name
-//      System.out.println(baron.name);         // 필드 중 name (접근제어자 private)
-
-        System.out.println("---");
-        System.out.println(aaron == baron);
+        System.out.println(aaron.toString());   // 객체
+//      aaron.setId(2);                         // final 변수는 초기화(Initialized) 이후 변경될 수 없다.
+//      aaron.setName("Baron");                 // final 변수는 초기화(Initialized) 이후 변경될 수 없다.
+        aaron.setAge(10);                       // Non-final 변수는 언제든 Setter 호출이 가능하다.
+        aaron.setEmail("aaron@example.com");    // Non-final 변수는 언제든 Setter 호출이 가능하다.
     }
 }
--- src/main/java/com/example/demo/member/Member.java
+++ src/main/java/com/example/demo/member/Member.java
@@ -1,22 +1,13 @@
 package com.example.demo.member;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
-import java.util.Objects;
-
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Setter
+@RequiredArgsConstructor
 public class Member {
-    private static class LazyHolder {
-        private static final Member UNIQUE_INSTANCE = new Member();
-    }
-
-    protected Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
-    protected String name;
-    protected int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
-    protected String email;
-
-    public static Member getInstance() {
-        return LazyHolder.UNIQUE_INSTANCE;
-    }
+    private final Integer id;
+    private final String name;
+    private int age;
+    private String email;
 }
