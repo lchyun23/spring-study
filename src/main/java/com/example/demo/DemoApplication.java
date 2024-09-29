fe47a8af033f58026a6499d0a55cc849c70ee5b8
Aaron Ryu <fbcndah@gmail.com>
2024-09-29 18:07:51 +0900

    실습 2-33: @Value - 주의 : final class, @FieldDefaults 추가 및 @Setter 삭제

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,14 +13,14 @@ public class DemoApplication {
         System.out.println("---");
         System.out.println(aaron);              // 객체
         System.out.println(aaron.toString());   // 객체
-        System.out.println(aaron.getName());    // @Getter (@Data = 모든 "FINAL" 필드에 대해 Getter 생성)
-//      aaron.setName("Caron");                 // @Setter (@Data = 모든 "NON-FINAL" 필드에 대해 Setter 생성) - 어떤것도 호출 불가
+        System.out.println(aaron.getName());    // @Getter (@Value = 모든 필드에 대해 Getter 생성)
+//      aaron.setName("Caron");                 // @Setter (@Value = Setter 미생성) - 어떤것도 호출 불가
 
         System.out.println("---");
         System.out.println(baron);              // 객체
         System.out.println(baron.toString());   // 객체
-        System.out.println(baron.getName());    // @Getter (@Data = 모든 "FINAL" 필드에 대해 Getter 생성)
-//      baron.setName("Daron");                 // @Setter (@Data = 모든 "NON-FINAL" 필드에 대해 Setter 생성) - 어떤것도 호출 불가
+        System.out.println(baron.getName());    // @Getter (@Value = 모든 필드에 대해 Getter 생성)
+//      baron.setName("Daron");                 // @Setter (@Value = Setter 미생성) - 어떤것도 호출 불가
 
         System.out.println("---");
         System.out.println(aaron == baron);
--- src/main/java/com/example/demo/member/Member.java
+++ src/main/java/com/example/demo/member/Member.java
@@ -2,9 +2,11 @@ package com.example.demo.member;
 
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
 
-@Data
-@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@Value
+@NonFinal                                                           // 상속(extends)을 위해서는 @NonFinal 통해 class 에게 자유를
+// @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)    // @Value 는 각 필드에 대한 본 어노테이션을 내재한다.
 public class Member {
     /* private final */ Integer id;
     /* private final */ String name;
