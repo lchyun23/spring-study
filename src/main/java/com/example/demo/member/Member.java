8b8d3313ec923ce32aa6cde598f6efc6861ac4b8
Aaron Ryu <fbcndah@gmail.com>
2024-09-29 15:00:36 +0900

    실습 2-10: @ToString - of 옵션 : 출력 필드 포함

--- src/main/java/com/example/demo/member/Member.java
+++ src/main/java/com/example/demo/member/Member.java
@@ -3,16 +3,14 @@ package com.example.demo.member;
 import lombok.*;
 
 @Getter
-@ToString
+@ToString(of = {"id", "name"})
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class Member {
     private Integer id;
     private String name;
     @Setter(AccessLevel.PRIVATE)
-    @ToString.Exclude
     private int age;
     @Setter
-    @ToString.Exclude
     private String email;
 
 //  public Member(Integer id, String name, int age, String email) {
