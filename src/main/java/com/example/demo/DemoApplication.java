75d8f34aa5518d3cc6feb74447fa5fe316f421d3
Aaron Ryu <fbcndah@gmail.com>
2024-09-29 22:21:17 +0900

    실습 3-6: Collection 3) Set<Element> - 빈(Empty) / 값 기반 선언

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,9 +6,7 @@ import com.example.demo.hero.StrengthHero;
 import com.example.demo.hero.common.Hero;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -43,34 +41,58 @@ public class DemoApplication {
 //      string_list.add("1");
 //      string_list.add("2");
 //      string_list.add("3");
-        List<String> string_list = Arrays.asList("1", "2", "3");    // 값 기반 List 선언 (String 요소)
-        string_list.get(3);                         // 조회
-        string_list.add("4");                       // 추가
-        string_list.set(3, "5");                    // 수정
-        string_list.remove(3);                      // 삭제
-        string_list.subList(0, 1);                  // 부분
-        string_list.sort(String::compareTo);        // 나열
-        string_list.contains("1");                  // 포함
-        string_list.clear();                        // 리셋
-        string_list.isEmpty();                      // 검사
-        string_list.size();                         // 개수
+//      List<String> string_list = Arrays.asList("1", "2", "3");    // 값 기반 List 선언 (String 요소)
+//      string_list.get(3);                         // 조회
+//      string_list.add("4");                       // 추가
+//      string_list.set(3, "5");                    // 수정
+//      string_list.remove(3);                      // 삭제
+//      string_list.subList(0, 1);                  // 부분
+//      string_list.sort(String::compareTo);        // 나열
+//      string_list.contains("1");                  // 포함
+//      string_list.clear();                        // 리셋
+//      string_list.isEmpty();                      // 검사
+//      string_list.size();                         // 개수
 //      List<Integer> integer_list = new ArrayList<>();             // 빈 List 선언 (Integer 요소)
 //      integer_list.add(1);
 //      integer_list.add(2);
 //      integer_list.add(3);
-        List<Integer> integer_list = Arrays.asList(1, 2, 3);        // 값 기반 List 선언 (Integer 요소)
-        integer_list.get(3);                        // 조회
-        integer_list.add(4);                        // 추가
-        integer_list.set(3, 5);                     // 수정
-        integer_list.remove(3);                     // 삭제
-        integer_list.subList(0, 1);                 // 부분
-        integer_list.sort(Integer::compareTo);      // 나열
-        integer_list.contains(1);                   // 포함
-        integer_list.clear();                       // 리셋
-        integer_list.isEmpty();                     // 검사
-        integer_list.size();                        // 개수
+//      List<Integer> integer_list = Arrays.asList(1, 2, 3);        // 값 기반 List 선언 (Integer 요소)
+//      integer_list.get(3);                        // 조회
+//      integer_list.add(4);                        // 추가
+//      integer_list.set(3, 5);                     // 수정
+//      integer_list.remove(3);                     // 삭제
+//      integer_list.subList(0, 1);                 // 부분
+//      integer_list.sort(Integer::compareTo);      // 나열
+//      integer_list.contains(1);                   // 포함
+//      integer_list.clear();                       // 리셋
+//      integer_list.isEmpty();                     // 검사
+//      integer_list.size();                        // 개수
 
-        System.out.println(string_list);
-        System.out.println(integer_list);
+        /* Collection 3) Set 세트 */
+//      Set<String> string_set = new HashSet<>();                  // 빈 Set 선언 (String 요소)
+//      string_set.add("1");
+//      string_set.add("2");
+//      string_set.add("3");
+        Set<String> string_set = new HashSet<>(Arrays.asList("1", "2", "3"));
+        string_set.add("4");                        // 추가
+        string_set.remove("1");                     // 삭제
+        string_set.contains("1");                   // 포함
+        string_set.clear();                         // 리셋
+        string_set.isEmpty();                       // 검사
+        string_set.size();                          // 개수
+//      Set<Integer> integer_set = new HashSet<>();                // 빈 Set 선언 (Integer 요소)
+//      integer_set.add(1);
+//      integer_set.add(2);
+//      integer_set.add(3);
+        Set<Integer> integer_set = new HashSet<>(Arrays.asList(1, 2, 3));
+        integer_set.add(4);                         // 추가
+        integer_set.remove(3);                      // 삭제
+        integer_set.contains(1);                    // 포함
+        integer_set.clear();                        // 리셋
+        integer_set.isEmpty();                      // 검사
+        integer_set.size();                         // 개수
+
+        System.out.println(string_set);
+        System.out.println(integer_set);
     }
 }
