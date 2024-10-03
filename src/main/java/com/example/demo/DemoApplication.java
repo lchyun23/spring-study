8c83c055b9e4ac64e1aa1b7d462fb56527e5c7dc
Aaron Ryu <fbcndah@gmail.com>
2024-10-03 18:03:06 +0900

    실습 3-22: Enum 미사용 시 - 수많은 파라미터 주입 가능

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,19 @@ import java.util.*;
 
 @SpringBootApplication
 public class DemoApplication {
+
+    public static void createMessage(String message) {
+        System.out.println("당신에게 메세지를 보냅니다. : " + message);
+    }
+
     public static void main(String[] args) {
+        createMessage("안녕하세요.");
+        createMessage("사랑합니다.");
+        createMessage("감사합니다.");
+        createMessage("안녕히가세요.");
+    }
+
+    public static void backup() {
         StrengthHeroRepository strengthHeroRepository = new StrengthHeroRepository();
         strengthHeroRepository.save(new StrengthHero("Dragon Knight"));
         strengthHeroRepository.save(new StrengthHero("Pudge"));
